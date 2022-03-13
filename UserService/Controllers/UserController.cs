65c54239ed1644fec491668a9d69559fba8299dc
angel <I412999@student.fontys.nl>
2022-03-13 13:12:01 +0100

    Add project files.

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserService.DBContexts;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// Database context for users, this is used to make calls to the database.
+        /// </summary>
+        private readonly UserServiceDatabaseContext _dbContext;
+
+        /// <summary>
+        /// Constructer is used for receiving the database context at the creation of the UserController.
+        /// </summary>
+        /// <param name="dbContext">Context of the database</param>
+        public UserController(UserServiceDatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Get all the Users from the database
+        /// </summary>
+        /// <returns>All Users in Db</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var result = await _dbContext.Users.ToListAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ UserService/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Models
+{
+    /// <summary>
+    /// <b>User</b> class used for EF Core to map its landscape for the database
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// [Key]: Identification number for a User entry in the User table
+        /// [Required]: cannot be null
+        /// Identification number
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Token used to refresh the login session of the user
+        /// </summary>
+        public string RefreshToken { get; set; }
+
+    }
+}
