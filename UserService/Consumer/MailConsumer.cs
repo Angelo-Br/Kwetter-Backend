a424323549d367e0eac1631476a70b6aea4db45b
Angelo Bruggemans <Angelo_B@live.nl>
2022-03-25 19:13:37 +0100

    Working version of rabbitmq but very ugly

--- UserService/Consumer/MailConsumer.cs
+++ UserService/Consumer/MailConsumer.cs
@@ -10,21 +10,29 @@ namespace UserService.Consumer
 
         public Task Boot(IModel _connection, string queueName )
         {
-            using var channel = _connection;
-            channel.ExchangeDeclare(EXCHANGE_NAME, "fanout", durable: true);
-            channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-            channel.QueueBind(queueName, EXCHANGE_NAME, routingKey: string.Empty);
-
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
+            using (var channel = _connection)
             {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] Received {0}", message);
-            };
-            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                channel.QueueDeclare(queue: queueName,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
 
-            return Task.CompletedTask;
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                return Task.CompletedTask;
+            }
         }
     }
 }
