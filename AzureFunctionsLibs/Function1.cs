5e3e1f6a4187060c2cb5e0e7d254d09605d93af9
I412999 <I412999@fhict.nl>
2022-05-26 13:51:20 +0200

    Added cloud service functionality

--- AzureFunctionsLibs/Function1.cs
+++ AzureFunctionsLibs/Function1.cs
@@ -7,29 +7,33 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
 
 namespace AzureFunctionsLibs
 {
     public static class Function1
     {
         [FunctionName("HttpExample")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task Run([TimerTrigger("*/15 * * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            // This entire azure function will have the purpose of updating the timeline of the site to the most trending topics
+            // for now for testing purpose it will simply change 1 value every 15 minutes
+            var str = Environment.GetEnvironmentVariable("kwetter_db_string").Replace("DATABASE_NAME", "timelineservice");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "UPDATE TimelineService.dbo.Timeline " +
+                        "SET [Updated] = GetDate() WHERE Updated < GetDate();";
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var rows = await cmd.ExecuteNonQueryAsync();
+                    log.LogInformation($"{rows} rows were updated");
+                }
+            }
         }
     }
 }
